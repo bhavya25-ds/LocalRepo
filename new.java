ec6d029729e6338743be1344e45da5473329094c
bhavya25-ds <bhavyakumar.2506@gmail.com>
2025-08-15 18:57:48 +0530

    Added a new line of code to check autosave

--- new.java
+++ new.java
@@ -3,5 +3,6 @@ package LocalRepo;
 public class new {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        system.out.println("This is a new Java file in the LocalRepo package.");
     }
 }
